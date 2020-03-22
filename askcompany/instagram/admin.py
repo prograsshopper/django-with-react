6a8f31deb3f8a12b0bf9326beb58abcb4582c970
prograsshopper <tjalwled12@gmail.com>
2020-03-22 23:46:12 +0900

    Models: lec.2 model field and django admin

--- askcompany/instagram/admin.py
+++ askcompany/instagram/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# admin.site.register(Post)
+
+@admin.register(Post) # Wrapping
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'message', 'is_public', 'created_at', 'updated_at']  # pk는 id의 alias
+    list_display_links = ['message']
+    search_fields = ['message']
+    list_filter = ['message', 'created_at', 'updated_at', 'is_public']
+
+    def message_length(self, post):
+        return len(post.message)
+
+# admin.site.register(Post, PostAdmin)
--- askcompany/instagram/models.py
+++ askcompany/instagram/models.py
@@ -3,5 +3,13 @@ from django.db import models
 
 class Post(models.Model):
     message = models.TextField()
+    is_public = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Custo object id : ({self.id})"
+    
+    # def message_length(self):
+    #     return len(self.message)
+    # message_length.short_description = "메세지 글자수"
