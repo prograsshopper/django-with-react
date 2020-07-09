d9da0f02156f5dd7b8ee8961432b62030fee40da
prograsshopper <tjalwled12@gmail.com>
2020-07-09 22:17:37 +0900

    Models: python manage.py shell_plus --print-sql

--- askcompany/instagram/models.py
+++ askcompany/instagram/models.py
@@ -11,6 +11,9 @@ class Post(models.Model):
     def __str__(self):
         return f"Custom object id : ({self.id})"
     
+    class Meta:
+        ordering = ['-id']
+    
     # def message_length(self):
     #     return len(self.message)
     # message_length.short_description = "메세지 글자수"
