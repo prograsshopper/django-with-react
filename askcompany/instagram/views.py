c17283b5e069f9df76869ea0f6935258846856f5
prograsshopper <tjalwled12@gmail.com>
2020-07-09 22:05:46 +0900

    Models: search function using queryset

--- askcompany/instagram/views.py
+++ askcompany/instagram/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+def post_list(request):
+    qs = Post.objects.all()
+    q = request.GET.get('q', '')
+    if q:
+        qs = qs.filter(message__icontains=q)
+    return render(request, 'instagram/post_list.html', {
+        'post_list': qs,
+        'q': q,
+    })
