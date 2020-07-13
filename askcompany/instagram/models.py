ab41c05a80d5d659a03f80a2ddb937c424573278
prograsshopper <tjalwled12@gmail.com>
2020-07-13 21:32:24 +0900

    Models: many to many field with Tag

--- askcompany/instagram/models.py
+++ askcompany/instagram/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     message = models.TextField()
     photo = models.ImageField(blank=True, upload_to='instagram/post/%Y/%m/%d')
+    tag_set = models.ManyToManyField('Tag', blank=True)
     is_public = models.BooleanField(default=False, verbose_name='공개여부')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -27,4 +28,9 @@ class Comment(models.Model):
     # post_id 라는 애가 생김
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)    
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    # post_set = models.ManyToManyField(Post)    
