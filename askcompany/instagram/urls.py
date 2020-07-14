f692b96635776dbad5476103c4db638cb99cda63
prograsshopper <tjalwled12@gmail.com>
2020-07-14 21:50:59 +0900

    Views: FBV part 1

--- askcompany/instagram/urls.py
+++ askcompany/instagram/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list),
+    path('<int:pk>/', views.post_detail),
 ]
--- askcompany/instagram/views.py
+++ askcompany/instagram/views.py
@@ -1,13 +1,23 @@
+from django.views.generic import ListView
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from .models import Post
 
-def post_list(request):
-    qs = Post.objects.all()
-    q = request.GET.get('q', '')
-    if q:
-        qs = qs.filter(message__icontains=q)
-    return render(request, 'instagram/post_list.html', {
-        'post_list': qs,
-        'q': q,
-    })
+post_list = ListView.as_view(model=Post)
+
+# def post_list(request):
+#     qs = Post.objects.all()
+#     q = request.GET.get('q', '')
+#     if q:
+#         qs = qs.filter(message__icontains=q)
+#     return render(request, 'instagram/post_list.html', {
+#         'post_list': qs,
+#         'q': q,
+#     })
+
+
+def post_detail(request: HttpResponse, pk: int) -> HttpResponse:
+    response = HttpResponse()
+    response.write("Hello World!")
+    return response
