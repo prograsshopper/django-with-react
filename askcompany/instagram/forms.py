a84cf5f79504258902ca72a786168265cd657207
prograsshopper <tjalwled12@gmail.com>
2020-09-21 12:05:49 +0900

    Forms: form validation

--- askcompany/instagram/forms.py
+++ askcompany/instagram/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 from .models import Post
 
@@ -8,3 +10,9 @@ class PostForm(forms.ModelForm):
         fields = [
             'message', 'photo', 'tag_set', 'is_public'
         ]
+    
+    def clean_message(self):
+        message = self.cleaned_data.get('message')
+        if message:
+            message = re.sub(r'[a-zA-Z]+', '', message)
+        return message
