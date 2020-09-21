7e58ad94f7ee592a0237444b6e2ad5614d3955ff
prograsshopper <tjalwled12@gmail.com>
2020-09-21 16:54:49 +0900

    Admin 빠진 부분 수정

--- askcompany/instagram/admin.py
+++ askcompany/instagram/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
-from .models import Post, Comment
+from .models import Post, Comment, Tag
 
 # admin.site.register(Post)
 
@@ -17,9 +17,14 @@ class PostAdmin(admin.ModelAdmin):
     def photo_tag(self, post):
         if post.photo:
             return mark_safe(f'<img src="{post.photo.url} style="width: 72px;" />"')
+        return None
 
 # admin.site.register(Post, PostAdmin)
 
 @admin.register
 class CommentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
