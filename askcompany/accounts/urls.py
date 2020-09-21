5e7b01863294e915affbd50d9095aece9ba9cc6e
prograsshopper <tjalwled12@gmail.com>
2020-09-21 18:42:12 +0900

    Basic Auth: login

--- askcompany/accounts/urls.py
+++ askcompany/accounts/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from django.contrib.auth.views import LoginView
+from django.urls import path
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+]
