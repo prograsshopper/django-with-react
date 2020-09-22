ee1880b9114d8ca733a43967a29daa1a8b1f5d41
prograsshopper <tjalwled12@gmail.com>
2020-09-22 14:33:21 +0900

    Basic Auth: Profile

--- askcompany/accounts/urls.py
+++ askcompany/accounts/urls.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/edit/', views.profile_edit, name='profile_edit'),
 ]
--- askcompany/accounts/views.py
+++ askcompany/accounts/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, UpdateView
 
-# Create your views here.
+from .models import Profile
+from .forms import ProfileForm
+
+# @login_required
+# def profile(request):
+#     return render(request, 'accounts/profile.html')
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/profile.html'
+
+
+profile = ProfileView.as_view()
+
+
+# class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+#     model = Profile
+#     form_class = ProfileForm
+
+
+# profile_edit = ProfileUpdateView.as_view()
+
+@login_required
+def profile_edit(request):
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = None
+    
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'accounts/profile_form.html', {
+        'form': form,
+    })
