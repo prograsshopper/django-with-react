33e03c059216dec708eb52af4967662e90732933
prograsshopper <tjalwled12@gmail.com>
2020-09-22 15:47:35 +0900

    Basic Auth: set quiz for login

--- askcompany/accounts/urls.py
+++ askcompany/accounts/urls.py
@@ -4,7 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('login/', LoginView.as_view(
+        template_name='accounts/login.html'
+        ), name='login'),
     # path('logout/'),
     path('profile/', views.profile, name='profile'),
     path('profile/edit/', views.profile_edit, name='profile_edit'),
