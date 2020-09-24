1c482d3c6b24e4515654ac3ed838f317f04f23a6
prograsshopper <tjalwled12@gmail.com>
2020-09-24 16:56:15 +0900

    DRF: JSON serializer

--- drf_project/instagram/serializers.py
+++ drf_project/instagram/serializers.py
@@ -1,9 +1,12 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from .models import Post
 
 
 class PostSerializer(ModelSerializer):
+    username = serializers.ReadOnlyField(source='author.username')
+    
     class Meta:
         model = Post
         fields = "__all__"
--- drf_project/instagram/views.py
+++ drf_project/instagram/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.viewsets import ModelViewSet
 from .serializers import PostSerializer
 from .models import Post
+
+
+class PostListAPIView(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
 
 class PostViewSet(ModelViewSet):
