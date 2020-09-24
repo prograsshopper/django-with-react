82a37361ba925e321cc5773f0a25a1421bb782bf
prograsshopper <tjalwled12@gmail.com>
2020-09-24 14:51:59 +0900

    DRF: write JSON response view

--- drf_project/instagram/urls.py
+++ drf_project/instagram/urls.py
@@ -1,4 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+
+router = DefaultRouter()
+router.register('post', views.PostViewSet) # 2개 URL을 만들어줌
+# router.urls # url pattern list
 
 urlpatterns = [
-    
+    path('', include(router.urls))
 ]
