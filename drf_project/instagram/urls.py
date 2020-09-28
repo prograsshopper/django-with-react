7390aa7943024a030cbe83bbfafa8489e65da6e8
prograsshopper <tjalwled12@gmail.com>
2020-09-28 16:45:42 +0900

    DRF: Renderer

--- drf_project/instagram/urls.py
+++ drf_project/instagram/urls.py
@@ -8,6 +8,7 @@ router.register('post', views.PostViewSet) # 2개 URL을 만들어줌
 # router.urls # url pattern list
 
 urlpatterns = [
-    path('public/', views.PublicPostListAPIView.as_view()),
+    path('mypost/<int:pk>/', views.PostDetailAPIView.as_view()),
+    # path('public/', views.PublicPostListAPIView.as_view()),
     path('', include(router.urls))
 ]
--- drf_project/instagram/views.py
+++ drf_project/instagram/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.renderers import TemplateHTMLRenderer
+
 from .serializers import PostSerializer
 from .models import Post
 
@@ -24,10 +28,22 @@ class PostViewSet(ModelViewSet):
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
     
-    @action(detail=False, methods=['PATCH'])
+    @action(detail=True, methods=['PATCH'])
     def set_public(self, request, pk):
         instance = self.get_object()
-        inst.is_public = True
+        instance.is_public = True
         instance.save(update_fields=['is_public'])
-        serializer = self.get_serializer(queryset, many=True)
+        serializer = self.get_serializer(instance)
         return Response(serializer.data)        
+
+
+class PostDetailAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "instagram/post_detail.html"
+
+    def get(self, request, *args, **kwargs):
+        post = self.get_object()
+        return Response({
+            'post': post,
+        })
