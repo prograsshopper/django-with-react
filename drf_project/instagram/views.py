44b8e6d6c84c4748e55319cb9f8fa938f7197bba
prograsshopper <tjalwled12@gmail.com>
2020-09-28 15:29:53 +0900

    DRF: ViewSet and Router

--- drf_project/instagram/views.py
+++ drf_project/instagram/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from .serializers import PostSerializer
 from .models import Post
 
@@ -16,3 +17,17 @@ class PublicPostListAPIView(APIView):
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    @action(detail=False, methods=['GET'])
+    def public(self, request):
+        queryset = self.get_queryset().filter(is_public=True)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=False, methods=['PATCH'])
+    def set_public(self, request, pk):
+        instance = self.get_object()
+        inst.is_public = True
+        instance.save(update_fields=['is_public'])
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)        
