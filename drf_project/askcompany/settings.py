a4e6c4aa225546d9c7fe6b4b9c736f54679f5c10
prograsshopper <tjalwled12@gmail.com>
2020-09-29 15:11:12 +0900

    DRF: throttling

--- drf_project/askcompany/settings.py
+++ drf_project/askcompany/settings.py
@@ -125,11 +125,15 @@ STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': [
-        'rest_framework.pagination.PageNumberPagination',
-    ],
     'PAGE_SIZE': 3,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '500/day',
+    }
 }
