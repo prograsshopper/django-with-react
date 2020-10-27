37ff4d013ced65083ee0535b43ea7f3443b184a7
prograsshopper <tjalwled12@gmail.com>
2020-10-27 17:44:40 +0900

    React: react element

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -3,18 +3,54 @@ import 'App.css';
 import { Button } from 'antd';
 
 
+class Counter1 extends React.Component {
+    state = {
+        value: this.props.initialValue,
+    }
 
+    onClick = () => {
+        const { value } = this.state;
+        this.setState({value: value + 1});
+    };
+
+    render() {
+        const { value } = this.state;
+        return (
+            <div>
+                Counter1: {value}
+                <Button onClick={this.onClick}>+1</Button>
+            </div>
+        ); 
+    }
+}
+
+
+class FruitComponent extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>좋아하는 과일</h1>
+                <ul>
+                    {
+                        this.props.fruits.map((name, index) => (
+                         <li key={index}>{name}</li>
+                        ))
+                    }
+                </ul>
+            </div>
+        );
+    }
+}
 
 function App() {
+    const fruits = ["사과", "바나나", "딸기"];
+
   return (
     <div>
-        <button>
-        Hello react
-        </button>
-
-        <Button type="primary" onClick={ () => console.log("clicked") }>
-            Hello Antd
-        </Button>
+        <div>
+            <Counter1 initialValue={10} />
+            <FruitComponent fruits={fruits} />
+        </div> 
     </div>
   );
 }
